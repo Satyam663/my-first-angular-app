3f62305b91cae465ecfb4c49a96bd022048cf97f
satyam663 <k.satyamjha@gmail.com>
2019-11-12 19:50:29 +0530

    url problem

--- my-first-angular-app/src/app/layout/admin/admin-routing.module.ts
+++ my-first-angular-app/src/app/layout/admin/admin-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { AddTransactionComponent } from './add-transaction/add-transaction.component';
+import { AllTransactionComponent } from './all-transaction/all-transaction.component';
+import { AllCustomerComponent } from './all-customer/all-customer.component';
 
 
 const routes: Routes = [
   {
     path: "",
     component: AdminComponent, 
-    // children: [
-    //   {path: 'admin', loadChildren: './layout/admin/admin.module#AdminModule'}
-    // ]
+    children: [
+      {path: 'addtransaction', component: AddTransactionComponent}
+    ]
+  },
+  // {
+  //   path: "add-transaction", component: AddTransactionComponent
+  // },
+  {
+    path: "all-transaction", component: AllTransactionComponent
+  },
+  {
+    path: "all-customer", component: AllCustomerComponent
   }
 ];
 
